ec98c40fe0bf369b8c96134f211c1da946372fa5
Gerard van Helden <gerard@van-helden.name>
2019-05-23 17:41:37 +0200

    add test script

---
+++ run.sh
@@ -0,0 +1,12 @@
+./build.sh
+
+java \
+    "-Dlog4j.configuration=file:./log4j.debugging.properties" \
+    -Xmx512M \
+    -Xms512M \
+    -cp 'lib/*:bin' \
+    nl.melp.linkchecker.LinkChecker \
+    --follow-from-local \
+    --threads=40 \
+    --ignore='/.*.pdf' \
+    $@ \
